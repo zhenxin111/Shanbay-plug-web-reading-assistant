24dc4640542b9647cdbccb1c24c13cb2665bab35
zhenxin111@163.com <zhenxin111@163.com>
2017-10-29 23:17:47 +0800

    双击单词翻译功能实现

--- README.md
+++ README.md
@@ -1,2 +1,33 @@
 # Shanbay-plug-web-reading-assistant
-Chrome扩展（扇贝）：实现网页阅读模式开启、按屏分页以及取词翻译功能
+## Chrome扩展：
+### 作用
+> 针对[卫报网站](https://www.theguardian.com)的文章单页，进行广告过滤、长页面分页以及双击取词翻译功能。
+
+
+
+### 具体功能实现
+
+#### 广告过滤：
+分析卫报网站文章页面布局，将广告标签一一移除；针对自动加载的谷歌广告，使用定时器（setInterval方法）每隔两秒进行检查删除；
+
+### 长页面分页：
++ 根据全文高度除以浏览器可见高度，向上取整数，为页数；
++ 分页跳转，利用标题和文章的元素的top值的设定改变，从而实现；
+
+### 双击取词翻译
++ 取词：利用document.getSelection().toString()方法得到所选单词；
++ 双击事件绑定，单击框外元素取消事件绑定；
++ 翻译：使用ajax向扇贝网提供的API进行请求，返回JSON并转换分析；
++ 创造弹出框：DOM以及CSS样式设定；
++ 框出框定位：根据，单词坐标加上弹出框尺寸是否超出浏览器可视区域，设置弹出框位置；
+
+
+参考文章：
++ [360扩展开发文档](http://open.chrome.360.cn/extension_dev/management.html)
++ [window.screen.height和window.screen.availHeight和document.body.clientHeight和document.documentElement.clientHeight](http://www.cnblogs.com/lwwen/p/7272698.html)
++ [JS clientHeight,scrollHeight,offsetHeight,scrollTop,offsetTop概念](http://www.cnblogs.com/zourong/p/4049012.html)
++ [stackoverflow上一个关于window.getSelection的回答](https://stackoverflow.com/search?q=window.getSelection)
++ [MDN关于Window.getSelection的解释](https://developer.mozilla.org/zh-CN/docs/Web/API/Window/getSelection)
+
+
+
--- js/shanbay.js
+++ js/shanbay.js
@@ -3,7 +3,9 @@
  */
 
 ;(function (){
-    //按广告标签筛选节点
+    /*
+    * 按广告标签筛选节点
+    * */
     var advertElement = ['#bannerandheader','.content__labels','#js-content-meta','.content__secondary-column','.content__meta-container','aside.element','.submeta','.submeta__share','.submeta__syndication','.content-footer ','footer.l-footer','.site-message','.js-ad-slot'];
 
     function handler(elems){
@@ -13,6 +15,7 @@
 
     }
     handler(advertElement);
+    scrollTo(0,0);
 
     //针对动态插入的广告，每隔两秒检查删除
     setInterval(function () {
@@ -25,8 +28,8 @@
     * 点击页数移动距离——由标题加上文章内容高度 乘 页数得到
     * */
     //var docH = document.body.scrollHeight;  //全文高度
-    var headerH = document.getElementsByTagName('header')[0].scrollHeight;  //全文高度
-    var contentH = document.getElementsByClassName('content__main')[0].scrollHeight;  //全文高度
+    var headerH = document.getElementsByTagName('header')[0].scrollHeight;  //标题高度
+    var contentH = document.getElementsByClassName('content__main')[0].scrollHeight;  //文章高度
     var docH = headerH + contentH;
     //var scrH = window.screen.height;        //屏幕高度
     var scrH = document.documentElement.clientHeight;        //浏览器实际可用高度
@@ -59,6 +62,7 @@
 
             //事件委托
             divCenter.addEventListener('click',function(e){
+
                 var target = e.target;
                 var n = parseInt(target.innerHTML);
 
@@ -93,6 +97,131 @@
 
     }
 
+
+    /*取词翻译
+    *双击单词查看翻译和发音
+    * */
+
+    document.addEventListener("dblclick",translate);
+    document.addEventListener("click",removePopup);//移除弹出框
+
+    function translate(e){
+        var e = e || window.event.target;
+        var word = window.getSelection().toString();    //利用getSelection()方法取词
+        var shanbay = 'https://api.shanbay.com/bdc/search/?word='+word;
+
+        var xhr = new XMLHttpRequest();
+        xhr.onreadystatechange = function (){
+            if(xhr.readyState == 4){
+                var msg = JSON.parse(xhr.responseText).data;
+                popup(e,msg);
+            }
+        }
+        xhr.open("get",shanbay,true);
+        xhr.send(null);
+
+    }
+
+
+
+    //创建弹出框
+    function popup(e,msg){
+        var pop = document.createElement("div");
+        pop.classList.add("pop-warp");
+
+        var word = document.createElement("h3");
+
+        var wordcontent = document.createElement("div");
+        wordcontent.classList.add("word-content");
+
+        var title = document.createElement("div");
+        title.classList.add("dictionary-title");
+
+        var comment = document.createElement("div");
+        comment.classList.add("dictionary-comment");
+
+        word.innerHTML = msg.content;
+
+        title.innerHTML = "<hr><div class=''><span>英</span><span><i class='iconfont' id='ico1'>&#xe608;<audio id='uk_audio' ></audio></i></span><span>[" + msg.pronunciations.uk + "]</span></div>" + "<div><span>美</span><span><i class='iconfont' id='ico2'>&#xe608;<audio id='us_audio' ></audio></i></span><span>[" + msg.pronunciations.us + "]</span></div>";
+
+        var defins = msg.definition.split('\n');
+        var com = '';
+        for(var i =0; i< defins.length;i++){
+            com += defins[i] + '<br>';
+        }
+        comment.innerHTML = com;
+
+
+        document.body.appendChild(pop);
+        pop.appendChild(word);
+        wordcontent.appendChild(title);
+        wordcontent.appendChild(comment);
+        pop.appendChild(wordcontent);
+
+
+        //弹出框内的点击避免最外层的事件委托
+        pop.addEventListener('click',function(e){
+            e.stopPropagation();
+        });
+        pop.addEventListener('dblclick',function(e){
+            e.stopPropagation();
+        })
+
+        document.getElementById("ico1").addEventListener('click', function (e) {
+
+            var audio = document.getElementById("uk_audio");
+            audio.src = msg.audio_addresses.uk[0];
+            audio.play();
+        });
+        document.getElementById("ico2").addEventListener('click', function (e) {
+            var audio = document.getElementById("us_audio");
+            audio.src = msg.audio_addresses.us[0];
+            audio.play();
+        })
+
+
+        popupStyle(e.pageX, e.pageY,pop);
+
+
+    }
+
+    //弹窗口样式以及位置
+    function popupStyle(x,y,pop){
+        var clientW = document.documentElement.clientWidth;
+        var clientH = document.documentElement.clientHeight;
+
+        var popW = pop.clientWidth;
+        var popH = pop.clientHeight;
+
+        pop.style.position = 'absolute';
+
+
+        if(x+popW > clientW){
+            pop.style.left = x - 24 - popW + "px";     //单词左
+            if(y+popH > clientH){
+                pop.style.top = y - 24 - popH + "px";  //单词上
+            }else{
+                pop.style.top = y + 24  + "px";       //单词下
+            }
+        }else{
+            pop.style.left = x + 24  + "px";    //单词右
+            if(y+popH > clientH){
+                pop.style.top = y - 24 - popH + "px";  //单词上
+            }else{
+                pop.style.top = y + 24  + "px";       //单词下
+            }
+        }
+    }
+
+
+    //移除弹出框
+    function removePopup(){
+        var pop = document.getElementsByClassName('pop-warp')[0];
+        if(pop){
+            document.body.removeChild(pop);
+        }
+    }
+    
 
 })();
 
